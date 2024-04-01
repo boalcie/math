873cdb81bf95c1073c325934775027c34d7df920
Danil Skalyanskiy <dskalyanskkiy@gmail.com>
2024-04-01 23:13:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double x = 8.2;
+double a = 2.2;
+double b = 8.2;
+double y = (Math.Exp(x-2.7)+3)/(x+1.3);
+double z = (y + 0.75 * Math.Abs(Math.Cos(b) + a))/(Math.Pow(y,2)+0.75 * Math.Abs(Math.Cos(b)));
+Console.WriteLine(z);
+Console.WriteLine(y);
